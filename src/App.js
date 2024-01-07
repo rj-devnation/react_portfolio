b4d54afc9ba59a4710bfdb95b465247a37355873
rj-devnation <rgeli90@gmail.com>
2024-01-07 01:20:25 -0600

    Added more description in the About section and created a button to expand and show my technical skills.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home'
 import './App.css';
 import './assets/fonts/fonts.css'
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../pages/Home/Home.css'
+import { SlArrowDown } from "react-icons/sl";
+
+
 
 const About = () => {
+    const [showTech, setShowTech] = useState(false);
+    const toggleShowTech = () => {
+        setShowTech(!showTech)
+    }
 
     return (
-        <div className='section-content'>
+        <div className='section-content about'>
             <div className='heading'>
                 About
             </div>
             <div className='about-me'>
-                Hi, I'm Roland. I graduated Business Information Technology at Red River College Polytechnic. 
+                {/* <span className='graduated'>Business Information Technology Diploma</span> */}
+                <span className='intro-name'>Hi, I'm Roland.</span>
+                <span className='intro-description'><p>I graduated <span className='graduated'>Business Information Technology</span> at Red River College Polytechnic. I intend to use my skills and knowledge in full stack development and work towards a career that will allow me to grow as a developer and bring creativity to my work.</p></span>
+            </div>
+
+            <div className='tech-section'>
+                <div className='view-tech' >
+                    View Technical Skills
+                </div>
+                <div className='arrow-button'>
+                    <SlArrowDown onClick={toggleShowTech}/>
+                </div>
+                <div className={`tech-skills ${showTech ? 'show' : ''}show`}>
+                    <ul>
+                        <li>React.js</li>
+                        <li>JavaScript</li>
+                        <li>HTML 5</li>
+                        <li>CSS</li>
+                        <li>Python</li>
+                        <li></li>
+                        <li>CSS</li>
+                        <li>CSS</li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
